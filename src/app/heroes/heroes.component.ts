9f43a89033d24cc785629daeb6928f2948c7233f
김동훈 <dongminic@gmail.com>
2018-02-05 22:32:45 +0900

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import{Component, OnInit}from '@angular/core';
+import {Hero}from '../hero';
+import {HEROES}from '../mock-heroes';
+import {HeroService }from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes : Hero[];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
